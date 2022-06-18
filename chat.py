eaaa4dd14ba13cab6fd45e6db3b97ad2e03eff78
Phai42 <107753073+Phai42@users.noreply.github.com>
2022-06-18 16:54:18 +0200

    [wip]: add chat

---
+++ chat.py
@@ -0,0 +1,18 @@
+import time
+zeitanfang = time.perf_counter()
+spieler = input()
+insect = "test"
+eingabe = ""
+dauer = 30
+insect = insect.lower()
+while not eingabe == insect:
+    eingabe = input()
+    eingabe = eingabe.lower()
+    print(spieler + ": " + eingabe)
+    if  zeitanfang > dauer:
+        #Fakt anzeigen
+        print("test erfogreich")
+        dauer = dauer * 2
+zeitende = time.perf_counter()
+print("Das Insekt wurde in " + str(round(zeitende - zeitanfang, 0)) + " Sekunden erraten.")
+    #Bild und Fakten anzeigen
